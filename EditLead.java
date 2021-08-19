c0327428ee84e3e8f28897339211085040cc7203
RamyaKrishnan004 <77857560+RamyaKrishnan004@users.noreply.github.com>
2021-08-19 20:48:33 +0530

    Add files via upload

---
+++ EditLead.java
@@ -0,0 +1,55 @@
+package week2assignments;
+
+import java.net.StandardSocketOptions;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EditLead {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ramya\\eclipse-workspace\\SeleniuAmAug\\Driver\\chromedriver.exe" );
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main"); 
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.id("ext-gen248")).sendKeys("ramya");
+		driver.findElement(By.id("ext-gen334")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[1]/a")).click();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.className("subMenuButton")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).clear();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("cts");
+		driver.findElement(By.className("smallSubmit")).click();
+		driver.close();
+		
+	}}		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+
